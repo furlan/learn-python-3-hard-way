98f5d9a8987bbe676756239b8ab98f0d0c6a0799
Flávio Furlan <furlan@abap101.com>
2021-09-05 22:15:03 -0500

    ex20

---
+++ ex20-1.py
@@ -0,0 +1,39 @@
+# import argv from sys module and bound locally
+from sys import argv
+
+# read the script input arguments
+script, input_file = argv
+
+# define a function to print the all lines of a text file passed in the parameter.
+def print_all(f):
+  print(f.read())
+
+# define a function to set the file cursor in the first line.
+def rewind(f):
+  f.seek(0)
+
+# define a function to print the content of actual cursor line.
+def print_a_line(line_count, f):
+  print(line_count, f.readline())
+
+# open the file passes in the scripts arguments
+current_file = open(input_file)
+
+print("First let's print the whole file:\n")
+
+print_all(current_file)
+
+print("Now let's rewind, kind of like a tape.")
+
+rewind(current_file)
+
+print("Let's print three lines:")
+
+current_line = 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
